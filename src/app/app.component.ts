073fdf85e38104863caed85c8cecad7b680fde51
Domenic Helfenstein <dom.helfenstein@gmail.com>
2023-10-26 13:17:52 +0200

    clean-up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+   selector: 'app-root',
+   template: `
+      <h1>Test <span class="blubb">Blubb</span></h1>
+      <style>
+         h1 {
+            .blubb {
+               color: green;
+            }
+         }
+      </style>
+   `,
+   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'ng-dez-2023';
 }
