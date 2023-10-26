e1b9fd1ac46ab6dd09f84c5ccedd980c41535dfd
Domenic Helfenstein <dom.helfenstein@gmail.com>
2023-10-26 15:43:16 +0200

    loggedIn.guard entfernt

--- src/auth.service.ts
+++ src/auth.service.ts
@@ -1,10 +1,25 @@
 import { Injectable, signal } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
+    constructor(
+        private router: Router
+    ) {
+    }
+
     public isLoggedIn = signal(false);
 
     public login() {
         this.isLoggedIn.set(true);
+    }
+
+    async canActivateBecauseItsLoggedIn() {
+        if (this.isLoggedIn()) {
+            return true;
+        }
+
+        await this.router.navigate(["login"]);
+        return false;
     }
 }
--- src/pages/main.route.ts
+++ src/pages/main.route.ts
@@ -1,13 +1,13 @@
 import { Route } from "@angular/router";
 import { dashboardRoute } from "./main-views/dashboard.routes";
-import { LoggedInGuard } from "../loggedIn.guard";
 import { inject } from "@angular/core";
+import { AuthService } from "../auth.service";
 
 export const mainRoute: Route =
     {
         path: "main",
         loadComponent: () => import("./main").then(file => file.MainPage),
-        canActivate: [() => inject(LoggedInGuard).canActivateBecauseItsLoggedIn()],
+        canActivate: [() => inject(AuthService).canActivateBecauseItsLoggedIn()],
         children: [
             dashboardRoute,
             {path: "", redirectTo: "dashboard", pathMatch: "full"}
