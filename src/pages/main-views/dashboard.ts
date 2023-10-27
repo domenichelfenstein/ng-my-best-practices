f3db0a60c2746f8e9d9ad92f6e31fac71873e706
Domenic Helfenstein <dom.helfenstein@gmail.com>
2023-10-27 10:20:02 +0200

    Navigation hover animation

--- src/pages/main-views/dashboard.ts
+++ src/pages/main-views/dashboard.ts
@@ -3,7 +3,18 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
 @Component({
     standalone: true,
     selector: `_dashboard`,
-    template: `Dashboard`,
+    template: `
+    <h1>Current Appointment</h1>
+    <div class="dashboard">
+       <section class="portrait"></section>
+       <section class="information"></section>
+       <section class="heart-rate"></section>
+       <section class="body-temperature"></section>
+       <section class="glucose"></section>
+       <section class="test-reports"></section>
+       <section class="prescriptions"></section>
+    </div>`,
+   styleUrls: ['./dashboard.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 
 })
--- src/pages/main.ts
+++ src/pages/main.ts
@@ -6,32 +6,38 @@ import { NgOptimizedImage } from "@angular/common";
     standalone: true,
     selector: "_main-page",
     template: `
-       <nav>
-          <div class="title"></div>
-          <a [routerLink]="['dashboard']" routerLinkActive="active">
-             <img ngSrc="../assets/icons/chess-knight.svg" height="28" alt="chess-knight" width="21"/>
-          </a>
-          <a [routerLink]="['users']" routerLinkActive="active">
-             <img ngSrc="../assets/icons/users.svg" height="28" alt="users" width="35" priority="" />
-          </a>
-          <a [routerLink]="['health']" routerLinkActive="active">
-             <img ngSrc="../assets/icons/heart-beat.svg" height="28" alt="heart" width="28"/>
-          </a>
-          <a [routerLink]="['docs']" routerLinkActive="active">
-             <img ngSrc="../assets/icons/document-text.svg" height="28" alt="docs" width="21"/>
-          </a>
-          <a [routerLink]="['calendar']" routerLinkActive="active">
-             <img ngSrc="../assets/icons/calendar.svg" height="28" alt="calendar" width="25"/>
-          </a>
-          <a [routerLink]="['archive']" routerLinkActive="active">
-             <img ngSrc="../assets/icons/folder.svg" height="28" alt="folder" width="28"/>
-          </a>
-       </nav>
-       <header></header>
-       <main>
-          <router-outlet></router-outlet>
-       </main>
-       <footer></footer>`,
+        <nav>
+            <div class="title"></div>
+            <a [routerLink]="['dashboard']" routerLinkActive="active">
+                <img ngSrc="../assets/icons/chess-knight.svg" height="28" alt="chess-knight" width="21"/>
+                <span>Dashboard</span>
+            </a>
+            <a [routerLink]="['users']" routerLinkActive="active">
+                <img ngSrc="../assets/icons/users.svg" height="28" alt="users" width="35" priority=""/>
+                <span>Users</span>
+            </a>
+            <a [routerLink]="['health']" routerLinkActive="active">
+                <img ngSrc="../assets/icons/heart-beat.svg" height="28" alt="heart" width="28"/>
+                <span>Health</span>
+            </a>
+            <a [routerLink]="['docs']" routerLinkActive="active">
+                <img ngSrc="../assets/icons/document-text.svg" height="28" alt="docs" width="21"/>
+                <span>Documents</span>
+            </a>
+            <a [routerLink]="['calendar']" routerLinkActive="active">
+                <img ngSrc="../assets/icons/calendar.svg" height="28" alt="calendar" width="25"/>
+                <span>Calendar</span>
+            </a>
+            <a [routerLink]="['archive']" routerLinkActive="active">
+                <img ngSrc="../assets/icons/folder.svg" height="28" alt="folder" width="28"/>
+                <span>Archive</span>
+            </a>
+        </nav>
+        <header></header>
+        <main>
+            <router-outlet></router-outlet>
+        </main>
+        <footer></footer>`,
     styleUrls: ["./main.scss"],
     imports: [
         RouterOutlet,
