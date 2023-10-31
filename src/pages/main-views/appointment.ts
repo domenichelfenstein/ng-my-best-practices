54f2fb79fe5390e03733bad4c7542128519cee50
Domenic Helfenstein <dom.helfenstein@gmail.com>
2023-10-31 07:59:37 +0100

    lazy loading dashboard widgets

--- src/pages/main-views/appointment.ts
+++ src/pages/main-views/appointment.ts
@@ -1,39 +1,26 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, effect, ViewChild, ViewContainerRef } from "@angular/core";
 import { PatientService } from "./patient.service";
 import { ProfileWidget } from "./appointment-widgets/profile";
 import { PatientInfoWidget } from "./appointment-widgets/patient-info";
-import { HeartRateWidget } from "./appointment-widgets/heart-rate";
-import { BodyTemperatureWidget } from "./appointment-widgets/body-temperature";
-import { GlucoseWidget } from "./appointment-widgets/glucose";
-import { TestReportsWidget } from "./appointment-widgets/test-reports";
-import { PrescriptionsWidget } from "./appointment-widgets/prescriptions";
-import { PatientInfoFromBackend, VitalsFromBackend } from "./appointment-widgets/fromBackend";
+import { PatientInfoFromBackend } from "./appointment-widgets/fromBackend";
+import { ActivatedRoute } from "@angular/router";
+import { toSignal } from "@angular/core/rxjs-interop";
 
 @Component({
    standalone: true,
    selector: `_appointment`,
    template: `
       <h1>Current Appointment</h1>
-      <div class="dashboard">
+      <div class="dashboard" #dashboard>
          <profile-widget fromBackend></profile-widget>
          <patient-info-widget fromBackend></patient-info-widget>
-         <heart-rate-widget fromBackend></heart-rate-widget>
-         <body-temperature-widget fromBackend></body-temperature-widget>
-         <glucose-widget fromBackend></glucose-widget>
-         <test-reports-widget></test-reports-widget>
-         <prescriptions-widget></prescriptions-widget>
+         <ng-container #placeholder></ng-container>
       </div>`,
    styleUrls: ['./appointment.scss'],
    changeDetection: ChangeDetectionStrategy.OnPush,
    imports: [
       PatientInfoWidget,
       ProfileWidget,
-      HeartRateWidget,
-      BodyTemperatureWidget,
-      GlucoseWidget,
-      TestReportsWidget,
-      PrescriptionsWidget,
-      VitalsFromBackend,
       PatientInfoFromBackend
    ],
    providers: [
@@ -41,4 +28,30 @@ import { PatientInfoFromBackend, VitalsFromBackend } from "./appointment-widgets
    ]
 })
 export class AppointmentView {
+   @ViewChild("placeholder", { read: ViewContainerRef }) dashboard!: ViewContainerRef;
+
+   constructor(
+      patientService: PatientService,
+      activatedRoute: ActivatedRoute,
+   ) {
+      const params = toSignal(activatedRoute.params);
+
+      setTimeout(() => {
+         import("./appointment-widgets/heart-rate").then(x => {
+            const widget = this.dashboard.createComponent(x.HeartRateWidget);
+            effect(() => widget.setInput("vital-signs", patientService.getContentCurrentPatientId(params, patientService.getVitalSigns)()), { injector: widget.injector });
+         });
+         import("./appointment-widgets/body-temperature").then(x => {
+            const widget = this.dashboard.createComponent(x.BodyTemperatureWidget);
+            effect(() => widget.setInput("vital-signs", patientService.getContentCurrentPatientId(params, patientService.getVitalSigns)()), { injector: widget.injector });
+         });
+         import("./appointment-widgets/glucose").then(x => {
+            const widget = this.dashboard.createComponent(x.GlucoseWidget);
+            effect(() => widget.setInput("vital-signs", patientService.getContentCurrentPatientId(params, patientService.getVitalSigns)()), { injector: widget.injector });
+         });
+
+         import("./appointment-widgets/test-reports").then(x => this.dashboard.createComponent(x.TestReportsWidget))
+         import("./appointment-widgets/prescriptions").then(x => this.dashboard.createComponent(x.PrescriptionsWidget))
+      }, 1000);
+   }
 }
